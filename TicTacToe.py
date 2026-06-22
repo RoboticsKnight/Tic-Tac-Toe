23e32b61a8a450a0b8af00c1a0f6fe5c5356564c
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-06-22 15:53:42 +0200

    Row, column and diagonal checks

--- TicTacToe.py
+++ TicTacToe.py
@@ -24,13 +24,14 @@ def display_board():
     print(board[6] + " | " + board[7] + " | " + board[8])
 
 # ======================================================================
-# Main function
+# Main function/Main Game control
 # ======================================================================
 def play_game():
 
     # Display initial board
     display_board()
 
+    # While the game is still going
     while game_still_going:
         # Handle a single turn of an arbitrary player
         handle_turn(current_player)
@@ -63,16 +64,114 @@ def handle_turn(player):
 # Game over checks
 # ======================================================================
 def check_if_game_over():
-    check_if_win()
+    check_for_winner()
     check_if_tie()
 
 # ======================================================================
 # Win checks
 # ======================================================================
-def check_if_win():
+def check_for_winner():
+
+    # Set up global variables
+    global winner
+
     # check rows
+    row_winner = check_rows()
+
     # check columns
+    column_winner = check_columns()
+
     # check diagonals    
+    diagonal_winner = check_diagonals()
+
+    if row_winner:
+        winner = row_winner()
+    elif column_winner:
+        winner = column_winner()
+    elif diagonal_winner:
+        winner = diagonal_winner()
+    else:
+        winner = None
+
+    return
+
+# ======================================================================
+# Rows check
+# ======================================================================
+def check_rows():
+
+    # Set up global variables
+    global game_still_going
+
+    # Check if any of the rows have all the same value (and is not empty)
+    row_1 = board[0] == board[1] == board[2] != "-"
+    row_2 = board[3] == board[4] == board[5] != "-"
+    row_3 = board[6] == board[7] == board[8] != "-"
+
+    # If any row does have a match, flag that there is a win
+    if row_1 or row_2 or row_3:
+        game_still_going = False
+    
+    # Return the winner (X or O)
+    if row_1:
+        return board[0]
+    if row_2:
+        return board[3]
+    if row_3:
+        return board[6]
+
+    return
+
+# ======================================================================
+# Columns check
+# ======================================================================
+def check_columns():
+    
+    # Set up global variables
+    global game_still_going
+
+    # Check if any of the columns have all the same value (and is not empty)
+    column_1 = board[0] == board[3] == board[6] != "-"
+    column_2 = board[1] == board[4] == board[7] != "-"
+    column_3 = board[2] == board[5] == board[8] != "-"
+
+    # If any column does have a match, flag that there is a win
+    if column_1 or column_2 or column_3:
+        game_still_going = False
+    
+    # Return the winner (X or O)
+    if column_1:
+        return board[0]
+    if column_2:
+        return board[1]
+    if column_3:
+        return board[2]
+
+    return
+
+# ======================================================================
+# Diagonals check
+# ======================================================================
+def check_diagonals():
+
+    # Set up global variables
+    global game_still_going
+
+    # Check if any of the diagonal has all the same value (and is not empty)
+    diagonals_1 = board[0] == board[4] == board[8] != "-"
+    diagonals_2 = board[6] == board[4] == board[2] != "-"
+
+
+    # If any diagonal does have a match, flag that there is a win
+    if diagonals_1 or diagonals_2:
+        game_still_going = False
+    
+    # Return the winner (X or O)
+    if diagonals_1:
+        return board[0]
+    if diagonals_2:
+        return board[6]
+
     return
 
 # ======================================================================
