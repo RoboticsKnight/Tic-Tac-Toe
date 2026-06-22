974bf7acf9e89505cb441585879f5f3cb9ab6a58
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-06-22 15:24:19 +0200

    Functions and conditional statements set up

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,6 +1,18 @@
+# ------- Global Variables ---------
+
+# Game board
 board = ["-","-","-",
          "-","-","-",
          "-","-","-"]
+
+# If Game is still going
+game_still_going = True
+
+# Who won? Or tie?
+winner = None
+
+# Whos turn is it
+current_player = "X"
 
 
 # ======================================================================
@@ -19,12 +31,26 @@ def play_game():
     # Display initial board
     display_board()
 
-    handle_turn()
+    while game_still_going:
+        # Handle a single turn of an arbitrary player
+        handle_turn(current_player)
+
+        # Check if the game has ended
+        check_if_game_over()
+
+        # Switch to the other player
+        switch_player()
+
+    # The game has ended
+    if winner == "X" or winner == "O":
+        print(winner + " won.")
+    elif winner == None:
+        print("Tie.")
 
 # ======================================================================
 # Player turn handling
 # ======================================================================
-def handle_turn():
+def handle_turn(player):
 
     # Player selecting where they would like to put their marker
     position = input("Choose a position from 1-9: ")
@@ -33,6 +59,33 @@ def handle_turn():
     board[position] = "X"
     display_board()
 
+# ======================================================================
+# Game over checks
+# ======================================================================
+def check_if_game_over():
+    check_if_win()
+    check_if_tie()
+
+# ======================================================================
+# Win checks
+# ======================================================================
+def check_if_win():
+    # check rows
+    # check columns
+    # check diagonals    
+    return
+
+# ======================================================================
+# Tie checks
+# ======================================================================
+def check_if_tie():
+    return
+
+# ======================================================================
+# Switching player handling
+# ======================================================================
+def switch_player():
+    return
 
 play_game()
 
