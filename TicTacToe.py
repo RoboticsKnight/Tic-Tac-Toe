ece3931e71f59d332e08523e6b550882c80c3b4b
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-06-22 14:54:23 +0200

    Empty board

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,1 +1,18 @@
-print("Hello World!")
+board = ["-","-","-",
+         "-","-","-",
+         "-","-","-"]
+
+
+# board
+# display board
+# play game
+# turn handling
+# check win
+    # check rows
+    # check columns
+    # check diagonals
+# check tie
+# switch player
+
+
+
