cee72c484f3bd8913ebf1cd4de543e913b788071
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2026-06-22 15:06:18 +0200

    Start of position and turn handling

--- TicTacToe.py
+++ TicTacToe.py
@@ -2,12 +2,42 @@ board = ["-","-","-",
          "-","-","-",
          "-","-","-"]
 
+
+# ======================================================================
+# Default board
+# ======================================================================
 def display_board():
     print(board[0] + " | " + board[1] + " | " + board[2])
     print(board[3] + " | " + board[4] + " | " + board[5])
     print(board[6] + " | " + board[7] + " | " + board[8])
 
-display_board()
+# ======================================================================
+# Main function
+# ======================================================================
+def play_game():
+
+    # Display initial board
+    display_board()
+
+    handle_turn()
+
+# ======================================================================
+# Player turn handling
+# ======================================================================
+def handle_turn():
+
+    # Player selecting where they would like to put their marker
+    position = input("Choose a position from 1-9: ")
+    position = int(position) - 1
+
+    board[position] = "X"
+    display_board()
+
+
+play_game()
+
+
+
 
 # board
 # display board
